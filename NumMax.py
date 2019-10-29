58383ded33cf34040f2e82ba5bc12d49e403d520
Richard Armando Rivero Ramirez <53186080+RichardRivero@users.noreply.github.com>
2019-10-29 08:30:17 -0400

    Documentación función valorMaximo()
    Se documento la función y se realizo en pruebas con la función doctest.testmod()

--- NumMax.py
+++ NumMax.py
@@ -1,7 +1,24 @@
 listaP = [1,5,3,7,25,9,8,1,12,15]
 
 def valorMaximo(lista1):
-    """ modulo que busca el maximo valor de una lista"""
+    """  Funcion valorMaximo()
+        modulo que busca el maximo valor de una listas o tuplas
+        pasandolo como parametro a la funcion.
+
+        Test:
+        
+        >>> listaP = [1,5,3,7,15,9,8,1,12,25]
+        >>> valorMaximo(listaP)
+        25
+
+        >>> listaP = (1,5,3,7,25,9,56,1,12,15)
+        >>> valorMaximo(listaP)
+        56
+
+        >>> listaP = (100,5,3,7,25,9,56,1,12,15)
+        >>> valorMaximo(listaP)
+        100    
+    """
     valorT = lista1[0]
     for num in lista1:
         
@@ -9,7 +26,10 @@ def valorMaximo(lista1):
             valorT = num
     return valorT
 
-print(valorMaximo(listaP))
+import doctest # importamos test de la funcion 
+doctest.testmod()# probamos si la funcion arroja lo que esperamos de la funcion
+
+
 
 
 
